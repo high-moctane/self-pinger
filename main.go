2d279893d2f1c7e45a7459497c5c69fb4b4cc703
high-moctane <high.moctane@gmail.com>
2019-11-09 19:39:39 +0900

    removed pinger package

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"flag"
+	"log"
+	"net/http"
 	"time"
-
-	"github.com/high-moctane/self-pinger/pinger"
 )
 
 var url = flag.String("u", "http://localhost", "target url")
@@ -12,6 +12,28 @@ var dur = flag.Duration("d", 20*time.Minute, "ping duration")
 
 func main() {
 	flag.Parse()
-	p := pinger.Pinger{URL: *url, Duration: *dur}
+	p := Pinger{URL: *url, Duration: *dur}
 	p.Run()
+}
+
+// Pinger fetches url with specified interval.
+type Pinger struct {
+	URL      string
+	Duration time.Duration
+}
+
+// Run starts pinging.
+func (p *Pinger) Run() {
+	c := time.Tick(p.Duration)
+	for range c {
+		resp, err := http.Get(p.URL)
+		if err != nil {
+			log.Println("pinger: ", err)
+			continue
+		}
+		log.Println("pinger: ping!")
+		if err = resp.Body.Close(); err != nil {
+			log.Println("pinger: ", err)
+		}
+	}
 }
